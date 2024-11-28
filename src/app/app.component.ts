ca9799f6b43cb83baa35aa6affc3a94eb13037b9
Marty Trujillo <marty@ilikeemail.com>
2024-11-28 08:32:54 -0500

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,41 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'three-19';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const width = window.innerWidth, height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+scene
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( width, height );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animate( time: number ) {
+
+	mesh.rotation.x = time / 2000;
+	//mesh.rotation.y = time / 1000;
+  //mesh.rotation.z = time / 4000;
+
+	renderer.render( scene, camera );
+
+}
+  }
+
 }
